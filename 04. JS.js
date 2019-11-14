813e9308aef2d791a51ea88b174d7200c423706f
agardner85 <39099693+agardner85@users.noreply.github.com>
2019-11-14 15:25:07 -0600

    Update 04. JS.js

--- 04. JS.js
+++ 04. JS.js
@@ -59,31 +59,199 @@
 /* --------------------------------------------- */
 /* --------------------------------------------- */
 
-      // Addition
-      var newNumber = 1+2; // =3
+        // Addition
+        var newNumber = 1+2; // =3
+        console.log(newNumber);
 
-      // Subtraction
-      var newNumber = 7-2; // =5
+        // Subtraction
+        var newNumber = 7-2; // =5
+        console.log(newNumber);
 
-      // Multiplication
-      var newNumber = 5*5; // =25
+        // Multiplication
+        var newNumber = 5*5; // =25
+        console.log(newNumber);
 
-      // Division
-      var newNumber = 10/2; // =10
+        // Division
+        var newNumber = 10/2; // =5
+        console.log(newNumber);
 
-      // order of operation / stuff in parentheses is calulated first
-      var newNumber = 5*5+((12-5)+(2*5));
-      // eq1=(12-5=7) 
-      // eq2=(2*5=10)
-      // eq3='eq1+eq2'=(7+10=12)
-      // eq4=(5*5=25)
-      // eq5='eq3+eq4'=(25+12=42)
-      // newNumber = 42
+        // order of operation / stuff in parentheses is calulated first
+        var newNumber = 5*5+((12-5)+(2*5));
+        // eq1=(12-5=7) 
+        // eq2=(2*5=10)
+        // eq3='eq1+eq2'=(7+10=12)
+        // eq4=(5*5=25)
+        // eq5='eq3+eq4'=(25+12=42)
+        // newNumber = 42
+        console.log(newNumber);
 
-      // if somthing true/false using greaterthan lessthan
-      var numOne = 5;
-      var numTwo = 10;
-      var numOneBigger = numOne > numTwo;
-      var numOneBigger2 = numOne < numTwo;
-      console.log(numOneBigger); // = false
-      console.log(numOneBigger2); // = true
+        // if somthing true/false using greaterthan lessthan
+        var numOne = 5;
+        var numTwo = 10;
+        var numOneBigger = numOne > numTwo;
+        var numOneBigger2 = numOne < numTwo;
+        console.log(numOneBigger); // = false
+        console.log(numOneBigger2); // = true
+
+/* --------------------------------------------- */
+/* --------------------------------------------- */
+/* --------------  If Statement  --------------- */
+/* --------------------------------------------- */
+/* --------------------------------------------- */     
+
+
+        // if statments will stop on the first statment that is true
+        // always add an 'else' to your fif statmenst. it seems like extra unneeded work, but it adds a level of fallback that allows you to find errors or redirect a probelms so that the codes does not die on that line.
+
+
+        // The below statment will stop on the first stament even though there is more written below it
+        var numOne = 5;
+        var numTwo = 8;
+        if (numOne == 5) {
+            console.log('this number is 5 | Statment 1 was satisfied and stoped');
+        }
+        else if (numTwo == 8) {
+            console.log('this number is 8 | Statment 1 was not satisifed but statment 2 was');
+        }
+        else {
+            console.log('this number is somthing other than 5 or 8');
+        }
+
+        // The below statment failed the firststamnet and was satissfied on the 2nd and stoped
+        var numOne = 5;
+        var numTwo = 8;
+        if (numOne == 9) {
+            console.log('this number is 5 | Statment 1 was satisfied and stoped');
+        }
+        else if (numTwo == 8) {
+            console.log('this number is 8 | Statment 1 was not satisfied but statment 2 was');
+        }
+        else {
+            console.log('this number is somthing other than 5 or 8 | no statments were satisfied');
+        }
+
+        // None of the statments were satisfied so it resulted into a generic catch all at the bottom of the statment.
+        var numOne = 5;
+        var numTwo = 8;
+        if (numOne == 9) {
+            console.log('this number is 5 | Statment 1 was satisfied and stoped');
+        }
+        else if (numTwo == 1) {
+            console.log('this number is 8 | statment 1 was not satisfied but statment 2 was');
+        }
+        else {
+            console.log('this number is somthing other than 5 or 8 | no statments were satisfied');
+        }
+
+/* --------------------------------------------- */
+/* --------------------------------------------- */
+/* ------  If Statement with Operators --------- */
+/* --------------------------------------------- */
+/* --------------------------------------------- */ 
+
+        // you can put multiple requirements into a single statment.
+
+
+        // statment one is false, but statment 2 has 2 requirements to fill to be satisfied
+        // && = and - this means that both statmenst need to be true
+        var numOne = 5;
+        var numTwo = 8;
+        if (numOne == 5 && numTwo == 8) {
+            console.log('all requirements were satisfied');
+        }
+        else {
+            console.log('this number is somthing other than 5 or 8');
+        }
+
+
+        // || = or - this means that only one of the requirmenst hass to be correct to be satisfied
+        var numOne = 5;
+        var numTwo = 8;
+        if (numOne == 5 || numTwo == 12) {
+            console.log('at least 1 of the requirements were satisfied');
+        }
+        else {
+            console.log('this number is somthing other than 5 or 8');
+        }
+
+
+        // ! = does not equal - This means that the requirmenst needs to be somthing other than the spacific value it is looking for.
+        var numOne = 5;
+        var numTwo = 8;
+        if (numOne != 4 && numTwo != 10) {
+            console.log('both requirmenst are somthing other than the spacific value it is looking for');
+        }
+        else {
+            console.log('-');
+        }
+
+
+        // > = greaterthan - the first requiment needs to be larger than the second
+        var numOne = 5;
+        var numTwo = 8;
+        if (numTwo > numOne) {
+            console.log('numTwo is larger than numOne');
+        }
+        else {
+            console.log('numTwo is smaller than numOne');
+        }
+
+
+        // >= = greaterthan or equal too - the first requiment needs to be the same as or larger that what it is being compared too.
+        var numOne = 5;
+        var numTwo = 8;
+        if (numTwo >= 8) {
+            console.log('The value is equal too or larger than numTwo');
+        }
+        else {
+            console.log('numTwo is smaller than the value is is looking for');
+        }
+
+
+        // < = greaterthan - the first requiment needs to be larger than the second
+        var numOne = 5;
+        var numTwo = 8;
+        if (numOne < numTwo) {
+            console.log('numOne is smaller than numTwo');
+        }
+        else {
+            console.log('numOne is larger than numTwo');
+        }
+
+
+        // <= = greaterthan or equal too - the first requiment needs to be the same as or larger that what it is being compared too.
+        var numOne = 5;
+        var numTwo = 8;
+        if (numOne <= 5) {
+            console.log('numOne is equal too or less than the value');
+        }
+        else {
+            console.log('numOne is larger than the value');
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
