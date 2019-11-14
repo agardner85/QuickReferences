4f4f9c3876cc3f82bc080b5aa89d83f60344ce8a
agardner85 <39099693+agardner85@users.noreply.github.com>
2019-11-14 13:10:55 -0600

    Update 04. JS.js

--- 04. JS.js
+++ 04. JS.js
@@ -10,7 +10,7 @@
         var newNum = 5; // number
 
     // How to set a variable version 'B'
-        var newVar, newVar2, newNum
+        var newVar, newVar2, newNum;
 
         // using one of the above variables at this time will return 'undefined' until you set a value to them
 
@@ -38,15 +38,15 @@
 
     var displayThisMessage = 'This is the message';
 
+    // This will present the information as a line item with in the inspector console
+        console.log(displayThisMessage);
+
     // This will display a pop up presented by the bowser. The user can not move forward until untill this is acknowledged
         alert(displayThisMessage);
-
-    // This will present the information as aline item with in the inspector console
-        console.log(displayThisMessage);
 
     // This will display a question to the user that much like the alert, must be addressed before the user can move on
         prompt('What day is today?');
 
-    // You would not do this unless you want to store/use the information so you can set it to a variable
-        var whatDayIsIt = prompt('What day is today?');
-        console.log(whatDayIsIt);
+        // You would not do this unless you want to store/use the information, so you can set it to a variable
+            var whatDayIsIt = prompt('What day is today?');
+            console.log(whatDayIsIt);
