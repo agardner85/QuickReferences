fab1628834b3b2a91a61dc3c6cf06ba06527a68c
agardner85 <39099693+agardner85@users.noreply.github.com>
2019-11-19 12:45:00 -0600

    Update 04. JS.js

--- 04. JS.js
+++ 04. JS.js
@@ -93,6 +93,13 @@
         console.log(numOneBigger); // = false
         console.log(numOneBigger2); // = true
 
+    // find out of a number is odd or even using a function.
+        function isOdd(num) { return num % 2;}
+        console.log("1 is " + isOdd(1));
+        console.log("2 is " + isOdd(2));
+        console.log("3 is " + isOdd(3));
+        console.log("4 is " + isOdd(4));
+
 /* --------------------------------------------- */
 /* --------------------------------------------- */
 /* --------------  If Statement  --------------- */
