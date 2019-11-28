312c3aa36988dce1daa63c359ecc09457e2b3e0a
agardner85 <39099693+agardner85@users.noreply.github.com>
2019-11-27 21:03:53 -0600

    Update 11. Python.py

--- 11. Python.py
+++ 11. Python.py
@@ -1,1 +1,121 @@
 
+Python - Interpreter
+
+install homebrew - https://brew.sh/
+terminal cmd: /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+install python
+terminal cmd: brew install python
+
+check python version
+terminal cmd: python --version // this will more than likly return python v2
+
+check puthon v3
+terminal cmd: python3 --version // this should return pythong v3 that is installed from homebrew
+
+to verify pythong is installed correctly you can drop down to the interpreter
+terminal cmd: python3
+
+
+
+
+
+
+
+
+
+//
+// must be in the python interpreter
+//
+
+how to rteturn a string
+terminal cmd: print("hi there")
+
+how to do sinple math
+terminal cmd: 4+6
+
+
+
+
+
+
+
+
+
+
+
+//
+// how to navigate terminal directories
+//
+
+how to get to a home directories
+terminal cmd: cd ~/Documents
+terminal cmd: cd ~/Desktop
+
+once you reach a home directory, you can then navigate the next level of folder with 'ls'
+terminal cmd: ls
+
+you can then type the next file or folder you wish to interact with
+terminal cmd: cd 'foldername'
+
+
+
+//
+// how to create a new file
+//
+
+terminal cmd: nano test.py
+
+
+//
+// nano shortcuts
+//
+
+ctrl+o = save
+ctrl+x = exit
+
+
+
+//
+// Python scripts
+//
+
+
+//
+// how to call a pythong script
+//
+
+
+terminal cmd: poython3 'file'
+terminal cmd: poython3 test.py
+
+
+
+
+
+//
+// Python Workspace
+//
+
+
+Install java - https://www.java.com/en/download/mac_download.jsp
+
+Install pycharm - https://www.jetbrains.com/pycharm/download/#section=mac
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
